dbe0f1b07b839f34253e46de41c73028714a6d1b
Amandine Sismondi <am.sismondi@gmail.com>
2022-10-24 16:47:32 +0200

    main nav padding added

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="main-header" v-once>
-        <img src="@/assets/images/craftCode-logo.png" alt="logo craft code">
+        <img class="logo" src="@/assets/images/craftCode-logo.png" alt="logo craft code">
         <nav class="top-nav">
             <router-link class="top-nav__item" to="/">Home</router-link>
             <router-link class="top-nav__item"  to="/projects">Projects</router-link>
@@ -19,5 +19,36 @@ export default {
 </script>
 
 <style scoped lang="scss">
- 
+ header {
+  height: 10vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  position: fixed;
+  z-index: 1000;
+}
+
+.logo {
+  height: 10vh;
+  margin: 0px;
+  &__image {
+    height: 100%;
+    text-align: left;
+  }
+}
+.top-nav {
+  margin: 0;
+  display: flex;
+  justify-content: space-between;
+    &__item {
+      height: 10vh;
+      padding: 30px;
+      color: #a39da9;
+      &:hover {
+        border-bottom: 2px #a39da9 solid;
+      }
+    }
+}
 </style>
