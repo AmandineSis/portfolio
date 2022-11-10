035d4c584a918c05452186c91525fd7ee89107d2
Amandine Sismondi <am.sismondi@gmail.com>
2022-11-10 14:57:54 +0100

    project-card__img resized

--- front/src/components/ProjectCard.vue
+++ front/src/components/ProjectCard.vue
@@ -5,7 +5,9 @@
       {{ projItems.subtitle }}
     </p>
     <img class="project__image" :src="projItems.screenshot" :alt="projItems.altTxt" />
-    <button class="project__btn" @click="$emit('openCard', projItems._id)">More details...</button>
+    <button class="project__btn" @click="$emit('openCard', projItems._id)">
+      More details...
+    </button>
   </div>
 </template>
 
@@ -38,13 +40,48 @@ export default {
     margin: 20px;
     color: #373638;
   }
+  &__description {
+    height: 50px;
+    margin: 20px;
+    color: #373638;
+  }
   &__image {
     display: inline-block;
     width: 200px;
+    height: 200px;
+    object-fit: contain;
   }
-  &__description {
-    margin: 20px;
-    color: #373638;
+  &__btn {
+    align-items: center;
+    background-color: #d83c65;
+    border-radius: 12px;
+    box-shadow: transparent 0 0 0 3px, rgba(18, 18, 18, 0.1) 0 6px 20px;
+    box-sizing: border-box;
+    color: #fff;
+    cursor: pointer;
+    display: inline-flex;
+    flex: 1 1 auto;
+    font-family: Inter, sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+    justify-content: center;
+    line-height: 1;
+    margin: 10px;
+    outline: none;
+    padding: 0.8rem 1.2rem;
+    text-align: center;
+    text-decoration: none;
+    transition: box-shadow 0.2s, -webkit-box-shadow 0.2s;
+    white-space: nowrap;
+    border: 0;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    &:hover {
+      box-shadow: #d83c65 0 0 0 3px, transparent 0 0 0 0;
+      color: #a39da9;
+      background-color: #fff;
+    }
   }
 }
 </style>
